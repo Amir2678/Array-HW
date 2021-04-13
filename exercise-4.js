81ca16cf7115f86ecc50aaa3495db133aa62eb41
Amir Hossein Kakavand <amirkkvnd99@gmail.com>
2021-04-13 14:45:58 +0430

    add exercise 4

---
+++ exercise-4.js
@@ -0,0 +1,4 @@
+var myString = 'jhk654jss123ddu_khs1632';
+
+console.log(myString.match(/\d+/g)); // تمام زیررشته هایی را که حداقل یک رقم
+                                     // دارند را پیدا می گند
